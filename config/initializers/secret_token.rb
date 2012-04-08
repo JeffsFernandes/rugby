5ade7b2930e801366434b445c986c12b938c28bd
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2012-04-07 23:04:05 -0300

    Create Project on Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rugby::Application.config.secret_token = 'dc6b1fd93d423cf8275702b4379bb0dcc75fd47ce31bd123b907e50c7c4be8b816d96d3d8f9fce8a6e14a5a4a1c83d3cfbfaddcbda5113e00a4e91ad38e93953'
